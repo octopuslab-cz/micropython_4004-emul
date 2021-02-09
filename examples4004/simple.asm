165469a10073642481635e1a8fb128f79b841846
Honza S. Copak <bonum.comunae@volny.cz>
2021-02-09 17:28:21 +0100

    added new i/o test

--- examples4004/simple.asm
+++ examples4004/simple.asm
@@ -1,12 +1,25 @@
-;run 'python main.py test.asm N'
-;where N is some value
-;program will calculate sum of 1 + 2 + ... + N
-;in registers r2 and r3
-;by default N = 5
+;ldm + print reg + sllep basic test
 
-ldm 12        ; acc = 12
+
+jms $3fa	    ; input.reg to acc + sleep
+xch r0              ; store it to r0
+jms $3fa	    ; input.reg to acc + sleep
+xch r1              ; store it to r1
+
+ldm 7               ; acc = 7
+jms $3fb            ; output acc + sleep
+iac		    ; increment Accumulator
+jms $3fb            ; output acc + sleep 
+
+main_loop:
+dac                 ; acc -= 1
+jms $3fb            ; output acc + sleep
+jcn az end          ; if acc == 0 jump to end 
+jun main_loop       ; jump to main_loop
+          
 end:
 
                     ; call custom subroutine for:
+jms $3fc            ; sleep
 jms $3ff            ; printing regs
 jms $3fd            ; printMemory(4, 16)
